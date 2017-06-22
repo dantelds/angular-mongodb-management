cf18526356d272ee671e85504b3586f18d5aa80e
dantelds <davidalejandro.idarragamartinez@amaris.com>
2017-06-22 15:21:06 +0200

    fix

--- backend/controllers/databases.js
+++ backend/controllers/databases.js
@@ -3,22 +3,13 @@ var mongoose = require('mongoose'),
 
 
 exports.getDatabases = function (req, res) {
-    //res.send({test:'test'}); 
-    
-   // mongoose.connect('mongodb://localhost/');
-    mongoose.connection.on('open', function(){
-        console.log('open');
+    mongoose.connection.once('open', function(){
     new Admin(mongoose.connection.db).listDatabases(function(err, result) {
             if(err) throw err;       
-                   console.log('result.databases------>', result.databases);
                    mongoose.connection.db.close(); 
                    res.send(result.databases); 
         });
     });    
-    mongoose.connection.on('disconnected', function(){
-            console.log('disconnected');
-            //next();
-    });
     mongoose.connect('mongodb://localhost/');
 };
 
