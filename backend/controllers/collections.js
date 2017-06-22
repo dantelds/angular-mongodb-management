725c86e95de18b27b3ec00703e71d87f63743394
dantelds <davidalejandro.idarragamartinez@amaris.com>
2017-06-22 22:43:17 +0200

    fix get collections

--- backend/controllers/collections.js
+++ backend/controllers/collections.js
@@ -3,9 +3,8 @@ var mongoose = require('mongoose');
 
 exports.getCollections = function (req, res) {
     mongoose.connection.once('open', function(){
-        console.log('open----->', mongoose.connection.db.collectionNames);
-        //res.send({});
-        mongoose.connection.db.collectionNames(function (err, collections) {
+       
+        mongoose.connection.db.listCollections().toArray(function (err, collections) {
                 console.log('soy las colleciones',collections); 
                 res.status(200).jsonp(collections);
                 mongoose.connection.close();    
